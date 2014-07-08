264f43bf808489f9162f2c99c4378d060633cea5
Rick Minerich <richard.minerich@gmail.com>
2014-07-08 16:33:36 -0400

    Added another simple generics test
    Signed-off-by: Rick Minerich <richard.minerich@gmail.com>

--- Barb.Tests/InteropGenericsTests.fs
+++ Barb.Tests/InteropGenericsTests.fs
@@ -20,4 +20,19 @@ let ``Barb should be able to call simple generic functions`` () =
     let func = new BarbFunc<unit,int>(pred, settings)
     let res = func.Execute()
     Assert.Equal(10, res)
+
+let returnsecond (a: 'a) (b: 'b) = b
+    
+[<Fact>]
+let ``Barb should be able to call simple generic functions with two parameters one generic`` () =
+    let namespaces = BarbSettings.Default.Namespaces |> Set.add "Barb.Tests.PredicateLanguageGenericInteropTests"
+    let settings = { BarbSettings.Default with Namespaces = namespaces } 
+
+    let pred = "returnsecond (10, \"Hello\")"
+
+    let func = new BarbFunc<unit,string>(pred, settings)
+    let res = func.Execute()
+    Assert.Equal<string>("Hello", res)
+
+
     
