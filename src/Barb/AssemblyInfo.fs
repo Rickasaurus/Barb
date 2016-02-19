2a5173d92d0b6c0320da8aa6d80c4fabfc09e44f
Rick Minerich <richard.minerich@gmail.com>
2016-02-19 17:20:38 -0500

    Verson number bump

--- src/Barb/AssemblyInfo.fs
+++ src/Barb/AssemblyInfo.fs
@@ -5,10 +5,10 @@ open System.Runtime.CompilerServices
 [<assembly: AssemblyTitleAttribute("Barb")>]
 [<assembly: AssemblyProductAttribute("Barb")>]
 [<assembly: AssemblyDescriptionAttribute("A Simple Dynamic Scripting Language for .NET")>]
-[<assembly: AssemblyVersionAttribute("1.0.1")>]
-[<assembly: AssemblyFileVersionAttribute("1.0.1")>]
+[<assembly: AssemblyVersionAttribute("1.0.2")>]
+[<assembly: AssemblyFileVersionAttribute("1.0.2")>]
 [<assembly: InternalsVisibleToAttribute("Barb.Tests")>]
 do ()
 
 module internal AssemblyVersionInformation =
-    let [<Literal>] Version = "1.0.1"
+    let [<Literal>] Version = "1.0.2"
