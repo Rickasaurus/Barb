a832f37c6483eb5ac2c4982e5c3c55294c23fe6e
Rick Minerich <richard.minerich@gmail.com>
2016-03-01 18:05:54 -0500

    Reintroduced Barb recursion tests

--- tests/Barb.Tests/ReduceTests.fs
+++ tests/Barb.Tests/ReduceTests.fs
@@ -8,7 +8,7 @@ open Barb.Representation
 
 open Xunit
 
-//[<Fact>]
+[<Fact>]
 let ``barb should properly handle recursion of summing 100000`` () = 
     let pred = """let recurse = fun i -> if i < 100000 then recurse (i + 1) else i in recurse 0"""
     let func = buildExpr<unit, int>(pred)
@@ -17,8 +17,8 @@ let ``barb should properly handle recursion of summing 100000`` () =
 
 type IntHolder = { Num: int }
 
-//[<Fact>]
-let ``barb should properly handle recursion of summing 10000 on final reduction`` () =
+[<Fact>]
+let ``barb should properly handle recursion of summing 100000 on final reduction`` () =
     let pred = """let recurse = fun i -> if i < 100000 then recurse (i + 1) else i in recurse Num"""
     let func = buildExpr<IntHolder, int>(pred)
     let res = func({ Num = 0 })
